853abb728f8651605cb445e0406b3cdd0b991540
user <Deepakshi.deeps@gmail.com>
2020-08-05 11:32:43 +0530

    Language App

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -16,12 +16,13 @@ public class WordAdapter extends ArrayAdapter<Word> {
     public WordAdapter(Activity context, ArrayList<Word> words) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
-        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
-        // going to use this second argument, so it can be any value. Here, we used 0.
+        // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not.
+        // going to use this second argument, so it can be any value. here, we used 0.
         super(context, 0, words);
     }
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         // Check if the existing view is being reused, otherwise inflate the view
         View listItemView = convertView;
         if(listItemView == null) {
--- app/src/main/java/com/example/android/miwok/color.java
+++ app/src/main/java/com/example/android/miwok/color.java
@@ -5,15 +5,33 @@ public class color {
 
     private String mMikowTransalation;
 
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    private static final int NO_IMAGE_PROVIDED = -1;
+
     public color(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMikowTransalation = miwokTranslation;
     }
+    public color(String defaultTranslation, String miwokTranslation, int imageResorceId) {
+        mDefaultTranslation = defaultTranslation;
+        mMikowTransalation = miwokTranslation;
+        mImageResourceId = imageResorceId;
+
+    }
+
     public String getDefaultTranslation() {
         return mDefaultTranslation;
     }
 
     public String getmiwokTranslation() {
         return mMikowTransalation;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
